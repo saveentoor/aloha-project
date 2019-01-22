cd909e18a90b9f53dbf87b3a74f68f987e6ab489
Saveen Toor <toorsaveen@gmail.com>
2019-01-22 08:34:44 -0800

    january 22, 2019 final project aloha

---
+++ Index.js
@@ -0,0 +1,32 @@
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top
+          },
+          1000
+        );
+        return false;
+      }
+    }
+  });
+});
+console.log("Hello World");
+
+$(".main-carousel").flickity({
+  // options
+  cellAlign: "left",
+  contain: true,
+  percentPosition: false,
+  imagesLoaded: true,
+  autoPlay: false,
+  prevNextButtons: false
+});
